27c3cefbe5f062f4b295112741174c53aed439f3
Mark Nelson <mark.x.nelson@oracle.com>
2019-03-30 14:34:31 -0400

    add some more tests

--- src/test/java/weblogic/logging/exporter/config/ConfigTest.java
+++ src/test/java/weblogic/logging/exporter/config/ConfigTest.java
@@ -8,13 +8,41 @@ package weblogic.logging.exporter.config;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Test the Config class")
 public class ConfigTest {
+
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+  private final PrintStream originalOut = System.out;
+  private final PrintStream originalErr = System.err;
+
+  private static final String EXPECTED_STRING =
+      "Config{weblogicLoggingIndexName='index1', publishHost='host1', "
+          + "publishPort=1234, weblogicLoggingExporterSeverity='Warning', "
+          + "weblogicLoggingExporterBulkSize='2', enabled=false, "
+          + "weblogicLoggingExporterFilters=[FilterConfig{expression='MSGID != 'BEA-000449'', "
+          + "servers=[]}], domainUID='domain1'}";
+
+  @BeforeEach
+  public void setUpStreams() {
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
+  }
+
+  @AfterEach
+  public void restoreStreams() {
+    System.setOut(originalOut);
+    System.setErr(originalErr);
+  }
 
   @DisplayName("Create the default config from an empty file")
   @Test
@@ -42,7 +70,7 @@ public class ConfigTest {
     // create config by loading an empty file
     Config config = Config.loadConfig(new File("src/test/resources/config1.yaml"));
 
-    // now check that the config contains the expected default values
+    // now check that the config contains the expected values
     assertAll(
         "config",
         () -> assertEquals("host1", config.getHost()),
@@ -58,5 +86,38 @@ public class ConfigTest {
                 config.getFilterConfigs().get(0).toString()),
         () -> assertEquals(2, config.getBulkSize()),
         () -> assertEquals("domain1", config.getDomainUID()));
+  }
+
+  @DisplayName("Config file does not exist")
+  @Test
+  public void configFileDoesNotExist() {
+    Config config = Config.loadConfig(new File("src/test/resources/no-such-file.yaml"));
+    assertTrue(outContent.toString().contains(("Not Found")));
+    assertTrue(outContent.toString().contains("Using default for all parameters"));
+  }
+
+  @DisplayName("Config file cannot be parsed")
+  @Test
+  public void configFileCannotBeParsed() {
+    Config config = Config.loadConfig(new File("src/test/resources/bad.yaml"));
+    assertTrue(outContent.toString().contains(("Error parsing configuration file")));
+    assertTrue(outContent.toString().contains("Using default for all parameters"));
+  }
+
+  @DisplayName("Should convert index name to lower case")
+  @Test
+  public void shouldConvertIndexNameToLowerCase() {
+    // create config by loading an empty file
+    Config config = Config.loadConfig(new File("src/test/resources/config2.yaml"));
+
+    // now check that the config contains the expected values
+    assertEquals("index2", config.getIndexName());
+  }
+
+  @DisplayName("Check the toString() method works as expected")
+  @Test
+  public void checkToStringWorksAsExpected() {
+    Config config = Config.loadConfig(new File("src/test/resources/config1.yaml"));
+    assertEquals(EXPECTED_STRING, config.toString());
   }
 }
